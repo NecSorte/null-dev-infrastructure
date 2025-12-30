da76490867997e074651d703fd7adddeb44cb88d
NecSorte <69991710+NecSorte@users.noreply.github.com>
2025-12-29 22:24:51 -0800

    Update SSH key paths and Proxmox node name
    updated

--- vars.tf
+++ vars.tf
@@ -1,21 +1,23 @@
 variable "ssh_public_key_path" {
-  default = "~/.ssh/id_rsa.pub"
+   description = "SSH public Key to inject via cloud-init"
+   type = string 
+   default = "~/.ssh/terraform_proxmox_ssh_key_nopassword.pub"
 }
 
 variable "proxmox_node" {
-    default = "node1"
+    default = "Node1"
 }
 
 variable "proxmox_dns" {
-    default = "127.0.0.1"
+#    default = "127.0.0.1"
 }
 
 variable "template_name" {
-    default = "ubuntu-focal-cloudinit-template"
+    default = "ubuntu-noble-cloud"
 }
 
 variable "ssh_private_key_path" {
-    default   = "~/.ssh/id_rsa"
+    default   = "~/.ssh/terraform_proxmox_ssh_key_nopassword"
     sensitive = true
 }
 
